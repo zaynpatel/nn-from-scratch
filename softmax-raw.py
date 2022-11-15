0b81401a32d7ddd1e4842b4045acbb62681e29b6
zaynpatel <65105835+zaynpatel@users.noreply.github.com>
2022-11-14 20:47:32 -0500

    Create softmax-raw.py

---
+++ softmax-raw.py
@@ -0,0 +1,20 @@
+import math 
+layer_outputs = [4.8, 1.21, 2.385]
+
+E = math.e
+
+exp_values = []
+
+for i in layer_outputs:
+    exp_values.append(E**i)
+
+print(exp_values)
+
+norm_base = sum(exp_values)
+norm_values = []
+
+for value in exp_values: 
+    norm_values.append(value / norm_base)
+
+print(norm_values)
+print(sum(norm_values))
